38df4189358bd01f145d8a52bac0ce294dbad36b
SAHIL SINGH <107126552+HYPERxFURY24@users.noreply.github.com>
2024-09-16 23:54:50 +0530

    Whatsapp API integration

---
+++ src/app/api/sendWhatsapp/route.jsx
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export const POST = async (req) => {
+    try {
+        const res = await axios.post('https://graph.facebook.com/v20.0/456878240832822/messages', {
+            'messaging_product': 'whatsapp',
+            'to': '918737063610',
+            'type': 'template',
+            'template': {"name":"hello_world", "language" : {"code":"en_US"}}
+        }, 
+        {
+            headers : {
+                'Authorization' : 'Bearer ' + process.env.access_token,
+                'Content-Type' : 'application/json',  
+            }
+        });
+        
+        return NextResponse.json({ success:true, data:res.data });
+    }
+    catch(err) {
+        console.error('Error sending message:', err.response?.data || err.message);
+        return NextResponse.json(
+            { success:false, message:'Error sending message on whatsapp' },
+            { status:500 }
+        );
+    }
+}
